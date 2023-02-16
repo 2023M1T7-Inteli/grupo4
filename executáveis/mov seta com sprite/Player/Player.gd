9c28d5807769d165c44edb724b40f563eedfee6b
Drielly <drielly.farias@sou.inteli.edu.br>
2023-02-16 13:22:45 -0300

    adicionei animacao

--- executáveis/mov seta com sprite/Player/Player.gd
+++ executáveis/mov seta com sprite/Player/Player.gd
@@ -12,6 +12,8 @@ onready var animationState = animationTree.get("parameters/playback")
 #acessa animation tree>root
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
+	#$AnimationPlayer.play()
+	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized() #comprimento do vector=1
