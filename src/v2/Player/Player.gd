8c8ecabb61579c7518105484edd422703c05a0a5
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-02-27 13:52:55 -0300

    jogo

--- src/v2/Player/Player.gd
+++ src/v2/Player/Player.gd
@@ -1,34 +1,20 @@
 extends KinematicBody2D
 
-func _ready():
-	$Sprite.visible = true
-	
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
-
-var velocity = Vector2.ZERO
-onready var animationPlayer = $AnimationPlayer
-# para não ter que criar outra função, tipo func_ready, use o onready
-onready var animationTree = $AnimationTree
-onready var animationState = animationTree.get("parameters/playback")
-#acessa animation tree>root
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	#$AnimationPlayer.play()
+	var vectorMovement = Vector2.ZERO
+	vectorMovement.x =Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	vectorMovement.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized() #comprimento do vector=1
-	
-	if input_vector != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	if(Input.is_action_pressed("ui_right")):
+		$AnimationPlayer.play("direita")
+	elif(Input.is_action_pressed("ui_left")):
+		$AnimationPlayer.play("esquerda")
+	elif(Input.is_action_pressed("ui_down")):
+		$AnimationPlayer.play("baixo")
+	elif(Input.is_action_pressed("ui_up")):
+		$AnimationPlayer.play("cima")
 	else:
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-			
-	velocity = move_and_slide(velocity)
-		
+		$AnimationPlayer.stop()
+	move_and_slide(vectorMovement*50)
+	
+
