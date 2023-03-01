134c72d1eebca60d13b6ca245989862f3200fdf5
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-03-01 11:12:43 -0300

    atualizações no mapa e comentários do código

--- src/v2/Player/Player.gd
+++ src/v2/Player/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
 func _physics_process(delta):
+	#Velocidade do jogador
 	var vectorMovement = Vector2.ZERO
+	#Definir a direção do personagem
 	vectorMovement.x =Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	vectorMovement.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+	#Muda a animação conforme a direção do personagem muda
 	if(Input.is_action_pressed("ui_right")):
 		$AnimationPlayer.play("direita")
 	elif(Input.is_action_pressed("ui_left")):
@@ -14,7 +16,9 @@ func _physics_process(delta):
 	elif(Input.is_action_pressed("ui_up")):
 		$AnimationPlayer.play("cima")
 	else:
+		#Para o personagem
 		$AnimationPlayer.stop()
+		#Movimenta o personagem
 	move_and_slide(vectorMovement*50)
 	
 
