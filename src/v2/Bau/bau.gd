2246dad92c80284f6b9ee035664174b02f9ace40
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-03-06 15:58:37 -0300

    frase

--- src/v2/Bau/bau.gd
+++ src/v2/Bau/bau.gd
@@ -36,3 +36,4 @@ func _process(delta) -> void:
 
 
 
+
---
+++ src/v2/world.gd
@@ -0,0 +1,7 @@
+extends Node2D
+#precarrega uma cena.
+var preloadMensagem = preload("res://Control.tscn").instance()
+
+func _on_Area2D_body_entered(body):
+	#chama uma cena flutuante
+	get_tree().current_scene.add_child(preloadMensagem)
