f55e346163241312ed689e2c625f063375ab3fe7
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-03-06 21:38:05 -0300

    placa da frase 1

--- src/v2/world.gd
+++ src/v2/world.gd
@@ -5,3 +5,6 @@ var preloadMensagem = preload("res://Control.tscn").instance()
 func _on_Area2D_body_entered(body):
 	#chama uma cena flutuante
 	get_tree().current_scene.add_child(preloadMensagem)
+	
+func _on_Area2D_body_exited(body):
+	get_tree().current_scene.remove_child(preloadMensagem)
