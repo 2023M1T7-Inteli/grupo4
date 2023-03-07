4275054f720be3f992050581a9e1fdb3a145ae97
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-03-07 08:56:34 -0300

    transição para a primeira fase

--- src/v2/world.gd
+++ src/v2/world.gd
@@ -2,15 +2,15 @@ extends Node2D
 #precarrega uma cena.
 var preloadMensagem = preload("res://Control.tscn").instance()
 
-func _on_Area2D_body_entered(body):
+func _on_placaTutorial_body_entered(body):
 	#chama uma cena flutuante
 	get_tree().current_scene.add_child(preloadMensagem)
 	
-func _on_Area2D_body_exited(body):
+func _on_placaTutorial_body_exited(body):
 	get_tree().current_scene.remove_child(preloadMensagem)
+	
 
 
-func _on_Area2D_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+
+func _on_PlacaFase1_body_entered(body):
 	get_tree().change_scene("res://Puzzle/Fase1/Mapa.tscn")
-	
-	
