59b27a4aae12ad9ffdfef752ca8f0104701a62f2
Drielly <drielly.farias@sou.inteli.edu.br>
2023-03-10 12:17:52 -0300

    gdd

---
+++ src/sprint 3/v2/Bau/bau.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+var playerRef 
+var x = false
+var isOpen = false
+
+func _on_bau_body_entered(body):
+	print(body)
+	x = true
+#
+	if body.is_in_group('Player'):
+		playerRef = body
+		x = true
+	
+func _on_bau_body_exited(body):
+	x = false
+	if body.is_in_group('player'):
+		playerRef = null
+		x = false
+		
+func _ready():
+	$AnimatedSprite.animation = 'fechado'
+	
+func switchChest():
+	if (isOpen == true):
+		$AnimatedSprite.animation = 'fechado'
+		isOpen = false
+	elif (isOpen == false):
+		$AnimatedSprite.animation = 'aberto'
+		isOpen = true
+	
+func _process(delta) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		if x == true:
+			switchChest()
+
+
+
+
+
---
+++ src/sprint 3/v2/Puzzle/Player/Player.gd
@@ -0,0 +1,83 @@
+extends KinematicBody2D
+
+var playing = false
+var tasks = []
+
+var velocity = Vector2()
+export (int) var speed = 72
+var tilemap = 32
+
+# Saves where player is going to
+onready var target = position
+
+# Saves if player is in motion
+onready var moving = false
+
+# Defines possible directions
+var directions = ['right', 'up', 'left', 'down']
+var currentDirection = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func rotate(command):
+	if (command == 'rotate_counter'):
+		if (currentDirection == 0):
+			currentDirection = 3
+		else:
+			currentDirection -= 1
+	elif (command == 'rotate'):
+		if (currentDirection == 3):
+			currentDirection == 0
+		else:
+			currentDirection += 1
+			
+func run(currentTask):
+	var x = tasks[currentTask]
+	
+	if (x == 'rotate' || x == 'rotate_counter'):
+		rotate(x)
+	if (x == 'walk'):
+		if (directions[currentDirection] == 'up'):
+			target.y -= tilemap
+			$Sprite.play("up")
+		elif (directions[currentDirection] == 'down'):
+			target.y += tilemap
+			$Sprite.play("down")
+		elif (directions[currentDirection] == 'right'):
+			target.x += tilemap
+			$Sprite.play("right")
+		elif (directions[currentDirection] == 'left'):
+			target.x -= tilemap
+			$Sprite.play("left")
+			
+		# Set player velocity according to target and speed
+		velocity = position.direction_to(target) * speed
+		
+	print(tasks[currentTask])
+	if (currentTask + 1 < len(tasks)):
+		yield(get_tree().create_timer(1.5), "timeout")
+		run(currentTask + 1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if (playing == true && len(tasks) >= 1):
+		run(0)
+		playing = false
+		# If the distance between the player and where it is going is more than 5
+	if (position.distance_to(target) > 5):
+		# Slide player to target
+		velocity = move_and_slide(velocity)
+		
+		moving = true
+		# If the distance is less than 5
+	else:
+		# Save that player is NOT moving
+		$Sprite.play("idle")
+		moving = false
+
+func _on_Play_pressed():
+	tasks = get_node("/root/Global").tasks
+	playing = true
+
