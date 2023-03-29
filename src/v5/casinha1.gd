3233add49e51b4204b91aa93f128d77623555193
IsabelleVOliveira <isabelle.oliveira@sou.inteli.edu.br>
2023-03-29 01:03:57 -0300

    efeitos de transição prontos

--- src/v5/casinha1.gd
+++ src/v5/casinha1.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 var i = false
 var x = false
+var start = false
 
-
+func _ready():
+	ScreenTransition.get_child(0).get_child(0).play("transition_in")
+	
 func _on_saidaCasinha1_body_entered(body):
 #Está função deixa a variavel x true para que quando aperte enter mude a cena.
 	x = true
@@ -11,7 +14,11 @@ func _on_saidaCasinha1_body_entered(body):
 func _on_saidaCasinha1_body_exited(body):
 #Está função deixa a variavel x para impossibilitar de mudar a cena.
 	x = false
-	
+	start == true
+	ScreenTransition.get_child(0).get_child(0).play("transition_out")
+	yield(get_tree().create_timer(1), "timeout")
+	get_tree().change_scene("res://HUB.tscn")
+	start == false
 func _process(delta):
 #Está função com que com que a varialvel i fique verdade fazendo com que a cena seja trocada.
 	if x == true and Input.is_action_just_pressed("ui_accept"):
--- src/v5/casinha2.gd
+++ src/v5/casinha2.gd
@@ -2,12 +2,21 @@ extends Node2D
 
 var x = false
 var i = false
+var start = false
+
+func _ready():
+	ScreenTransition.get_child(0).get_child(0).play("transition_in")
 
 func _on_saida2_body_entered(body):
 	x = true
 
 func _on_saida2_body_exited(body):
 	x = false
+	start == true
+	ScreenTransition.get_child(0).get_child(0).play("transition_out")
+	yield(get_tree().create_timer(1), "timeout")
+	get_tree().change_scene("res://HUB.tscn")
+	start == false
 
 func _process(delta):
 	if x == true and Input.is_action_just_pressed("ui_accept"):
