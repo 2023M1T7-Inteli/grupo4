310365a82a8f9ea1b5e84ab212aceda50d7099d1
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-04-05 14:46:12 -0300

    interior de todas casas prontos
    foi adicionado dentro de todas casas moveis.

--- src/v5/casas/casinha4.gd
+++ src/v5/casas/casinha4.gd
@@ -12,6 +12,12 @@ func _on_Area2D_body_exited(body):
 	x = false
 	
 func _process(delta):
+	if newDialog.has_current_dialog_node() == true:
+		get_node("Player").moving = false
+		self.pause_mode= Node.PAUSE_MODE_STOP
+	else:
+		get_node("Player").moving = true
+		self.pause_mode= Node.PAUSE_MODE_INHERIT
 	if x == true and Input.is_action_just_pressed("ui_accept"):
 		i = true
 
